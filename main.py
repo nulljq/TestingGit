e045af34a15be7e0f8adf72ccbfe79b94e72d802
nulljq <nulljq@gmail.com>
2024-03-28 16:20:21 +0400

    Second

---
+++ main.py
@@ -0,0 +1,4 @@
+for i in range(10):
+    print("I made to test Git")
+for i in range(20):
+    print("I made it to the test git again")
---
+++ main22.py
@@ -0,0 +1,9 @@
+i = 0
+while i<10:
+    print("I made to test Git")
+    i+=1
+
+a = 0
+while a<20:
+    print("I made to test Git again")
+    a+=1
